a94648ada983a5eae56eb37e02afbd4568f72230
Mohammed Mustafa <mohammed.mustafatz@inxiteout.ai>
2023-04-19 14:27:13 +0530

    dasboard_navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,98 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
 function Navigation() {
+  const [isToggled, setIsToggled] = useState(false);
+
+  useEffect(() => {
+    const sidebarToggle = document.querySelectorAll('#sidebarToggle, #sidebarToggleTop');
+    const sidebarCollapse = document.querySelectorAll('.sidebar .collapse');
+    const body = document.querySelector('body');
+    const sidebar = document.querySelector('.sidebar');
+    const scrollToTop = document.querySelectorAll('a.scroll-to-top');
+    const windowWidth = window.innerWidth;
+
+    // Toggle the side navigation
+    sidebarToggle.forEach((toggle) => {
+      toggle.addEventListener('click', () => {
+        setIsToggled(!isToggled);
+        body.classList.toggle('sidebar-toggled');
+        sidebar.classList.toggle('toggled');
+        if (sidebar.classList.contains('toggled')) {
+          sidebarCollapse.forEach((collapse) => {
+            collapse.classList.remove('show');
+          });
+        }
+      });
+    });
+
+    // Close any open menu accordions when window is resized below 768px
+    window.addEventListener('resize', () => {
+      if (windowWidth < 768) {
+        sidebarCollapse.forEach((collapse) => {
+          collapse.classList.remove('show');
+        });
+      }
+
+      // Toggle the side navigation when window is resized below 480px
+      if (windowWidth < 480 && !sidebar.classList.contains('toggled')) {
+        body.classList.add('sidebar-toggled');
+        sidebar.classList.add('toggled');
+        sidebarCollapse.forEach((collapse) => {
+          collapse.classList.remove('show');
+        });
+      }
+    });
+
+    // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+    if (body.classList.contains('fixed-nav')) {
+      sidebar.addEventListener('mousewheel DOMMouseScroll wheel', (e) => {
+        if (windowWidth > 768) {
+          const e0 = e.originalEvent;
+          const delta = e0.wheelDelta || -e0.detail;
+          sidebar.scrollTop += (delta < 0 ? 1 : -1) * 30;
+          e.preventDefault();
+        }
+      });
+    }
+
+    // Scroll to top button appear
+    window.addEventListener('scroll', () => {
+      const scrollDistance = window.scrollY;
+      scrollToTop.forEach((link) => {
+        if (scrollDistance > 100) {
+          link.classList.add('show');
+        } else {
+          link.classList.remove('show');
+        }
+      });
+    });
+
+    // Smooth scrolling using jQuery easing
+    scrollToTop.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        const anchor = link.getAttribute('href');
+        const offsetTop = document.querySelector(anchor).offsetTop;
+        window.scroll({
+          top: offsetTop,
+          behavior: 'smooth',
+        });
+        e.preventDefault();
+      });
+    });
+  }, [])
+
   return (
-    <div>
+    <div className='googlesans'>
   {/* Page Wrapper */}
   <div id="wrapper">
     {/* Sidebar */}
     <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
       {/* Sidebar - Brand */}
-      <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div className="sidebar-brand-icon rotate-n-15">
+      <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+        {/*<div className="sidebar-brand-icon rotate-n-15">
           <i className="fas fa-laugh-wink" />
-        </div>
-        <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        </div>*/}
+        <div className="sidebar-brand-text mx-3">Inxite Out</div>
       </a>
       {/* Divider */}
       <hr className="sidebar-divider my-0" />
@@ -30,31 +110,31 @@ function Navigation() {
       </div>
       {/* Nav Item - Pages Collapse Menu */}
       <li className="nav-item">
-        <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+        <a className="nav-link collapsed" href="/" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
           <i className="fas fa-fw fa-cog" />
           <span>Components</span>
         </a>
         <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
           <div className="bg-white py-2 collapse-inner rounded">
             <h6 className="collapse-header">Custom Components:</h6>
-            <a className="collapse-item" href="buttons.html">Buttons</a>
-            <a className="collapse-item" href="cards.html">Cards</a>
+            <a className="collapse-item" href="/">Buttons</a>
+            <a className="collapse-item" href="/">Cards</a>
           </div>
         </div>
       </li>
       {/* Nav Item - Utilities Collapse Menu */}
       <li className="nav-item">
-        <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+        <a className="nav-link collapsed" href="/" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
           <i className="fas fa-fw fa-wrench" />
           <span>Utilities</span>
         </a>
         <div id="collapseUtilities" className="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
           <div className="bg-white py-2 collapse-inner rounded">
             <h6 className="collapse-header">Custom Utilities:</h6>
-            <a className="collapse-item" href="utilities-color.html">Colors</a>
-            <a className="collapse-item" href="utilities-border.html">Borders</a>
-            <a className="collapse-item" href="utilities-animation.html">Animations</a>
-            <a className="collapse-item" href="utilities-other.html">Other</a>
+            <a className="collapse-item" href="/">Colors</a>
+            <a className="collapse-item" href="/">Borders</a>
+            <a className="collapse-item" href="/">Animations</a>
+            <a className="collapse-item" href="/">Other</a>
           </div>
         </div>
       </li>
@@ -66,47 +146,35 @@ function Navigation() {
       </div>
       {/* Nav Item - Pages Collapse Menu */}
       <li className="nav-item">
-        <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+        <a className="nav-link collapsed" href="/" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
           <i className="fas fa-fw fa-folder" />
           <span>Pages</span>
         </a>
         <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
           <div className="bg-white py-2 collapse-inner rounded">
             <h6 className="collapse-header">Login Screens:</h6>
-            <a className="collapse-item" href="login.html">Login</a>
-            <a className="collapse-item" href="register.html">Register</a>
-            <a className="collapse-item" href="forgot-password.html">Forgot Password</a>
+            <a className="collapse-item" href="/">Login</a>
+            <a className="collapse-item" href="/">Register</a>
+            <a className="collapse-item" href="/">Forgot Password</a>
             <div className="collapse-divider" />
             <h6 className="collapse-header">Other Pages:</h6>
-            <a className="collapse-item" href="404.html">404 Page</a>
-            <a className="collapse-item" href="blank.html">Blank Page</a>
+            <a className="collapse-item" href="/">404 Page</a>
+            <a className="collapse-item" href="/">Blank Page</a>
           </div>
         </div>
       </li>
       {/* Nav Item - Charts */}
       <li className="nav-item">
-        <a className="nav-link" href="charts.html">
+        <a className="nav-link" href="/">
           <i className="fas fa-fw fa-chart-area" />
           <span>Charts</span></a>
       </li>
       {/* Nav Item - Tables */}
       <li className="nav-item">
-        <a className="nav-link" href="tables.html">
+        <a className="nav-link" href="/">
           <i className="fas fa-fw fa-table" />
           <span>Tables</span></a>
       </li>
-      {/* Divider */}
-      <hr className="sidebar-divider d-none d-md-block" />
-      {/* Sidebar Toggler (Sidebar) */}
-      <div className="text-center d-none d-md-inline">
-        <button className="rounded-circle border-0" id="sidebarToggle" />
-      </div>
-      {/* Sidebar Message */}
-      <div className="sidebar-card d-none d-lg-flex">
-        <img className="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="..." />
-        <p className="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
-        <a className="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
-      </div>
     </ul>
     {/* End of Sidebar */}
     {/* Content Wrapper */}
@@ -116,7 +184,7 @@ function Navigation() {
         {/* Topbar */}
         <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
           {/* Sidebar Toggle (Topbar) */}
-          <button id="sidebarToggleTop" className="btn btn-link d-md-none rounded-circle mr-3">
+          <button id="sidebarToggleTop" className="btn btn-link rounded-circle mr-3">
             <i className="fa fa-bars" />
           </button>
           {/* Topbar Search */}
@@ -134,7 +202,7 @@ function Navigation() {
           <ul className="navbar-nav ml-auto">
             {/* Nav Item - Search Dropdown (Visible Only XS) */}
             <li className="nav-item dropdown no-arrow d-sm-none">
-              <a className="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <a className="nav-link dropdown-toggle" href="/" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i className="fas fa-search fa-fw" />
               </a>
               {/* Dropdown - Messages */}
@@ -153,7 +221,7 @@ function Navigation() {
             </li>
             {/* Nav Item - Alerts */}
             <li className="nav-item dropdown no-arrow mx-1">
-              <a className="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <a className="nav-link dropdown-toggle" href="/" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i className="fas fa-bell fa-fw" />
                 {/* Counter - Alerts */}
                 <span className="badge badge-danger badge-counter">3+</span>
@@ -163,7 +231,7 @@ function Navigation() {
                 <h6 className="dropdown-header">
                   Alerts Center
                 </h6>
-                <a className="dropdown-item d-flex align-items-center" href="#">
+                <a className="dropdown-item d-flex align-items-center" href="/">
                   <div className="mr-3">
                     <div className="icon-circle bg-primary">
                       <i className="fas fa-file-alt text-white" />
@@ -174,7 +242,7 @@ function Navigation() {
                     <span className="font-weight-bold">A new monthly report is ready to download!</span>
                   </div>
                 </a>
-                <a className="dropdown-item d-flex align-items-center" href="#">
+                <a className="dropdown-item d-flex align-items-center" href="/">
                   <div className="mr-3">
                     <div className="icon-circle bg-success">
                       <i className="fas fa-donate text-white" />
@@ -185,7 +253,7 @@ function Navigation() {
                     $290.29 has been deposited into your account!
                   </div>
                 </a>
-                <a className="dropdown-item d-flex align-items-center" href="#">
+                <a className="dropdown-item d-flex align-items-center" href="/">
                   <div className="mr-3">
                     <div className="icon-circle bg-warning">
                       <i className="fas fa-exclamation-triangle text-white" />
@@ -196,12 +264,12 @@ function Navigation() {
                     Spending Alert: We've noticed unusually high spending for your account.
                   </div>
                 </a>
-                <a className="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+                <a className="dropdown-item text-center small text-gray-500" href="/">Show All Alerts</a>
               </div>
             </li>
             {/* Nav Item - Messages */}
             <li className="nav-item dropdown no-arrow mx-1">
-              <a className="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <a className="nav-link dropdown-toggle" href="/" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i className="fas fa-envelope fa-fw" />
                 {/* Counter - Messages */}
                 <span className="badge badge-danger badge-counter">7</span>
@@ -211,7 +279,7 @@ function Navigation() {
                 <h6 className="dropdown-header">
                   Message Center
                 </h6>
-                <a className="dropdown-item d-flex align-items-center" href="#">
+                <a className="dropdown-item d-flex align-items-center" href="/">
                   <div className="dropdown-list-image mr-3">
                     <img className="rounded-circle" src="img/undraw_profile_1.svg" alt="..." />
                     <div className="status-indicator bg-success" />
@@ -222,7 +290,7 @@ function Navigation() {
                     <div className="small text-gray-500">Emily Fowler · 58m</div>
                   </div>
                 </a>
-                <a className="dropdown-item d-flex align-items-center" href="#">
+                <a className="dropdown-item d-flex align-items-center" href="/">
                   <div className="dropdown-list-image mr-3">
                     <img className="rounded-circle" src="img/undraw_profile_2.svg" alt="..." />
                     <div className="status-indicator" />
@@ -233,7 +301,7 @@ function Navigation() {
                     <div className="small text-gray-500">Jae Chun · 1d</div>
                   </div>
                 </a>
-                <a className="dropdown-item d-flex align-items-center" href="#">
+                <a className="dropdown-item d-flex align-items-center" href="/">
                   <div className="dropdown-list-image mr-3">
                     <img className="rounded-circle" src="img/undraw_profile_3.svg" alt="..." />
                     <div className="status-indicator bg-warning" />
@@ -244,7 +312,7 @@ function Navigation() {
                     <div className="small text-gray-500">Morgan Alvarez · 2d</div>
                   </div>
                 </a>
-                <a className="dropdown-item d-flex align-items-center" href="#">
+                <a className="dropdown-item d-flex align-items-center" href="/">
                   <div className="dropdown-list-image mr-3">
                     <img className="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60" alt="..." />
                     <div className="status-indicator bg-success" />
@@ -255,32 +323,32 @@ function Navigation() {
                     <div className="small text-gray-500">Chicken the Dog · 2w</div>
                   </div>
                 </a>
-                <a className="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
+                <a className="dropdown-item text-center small text-gray-500" href="/">Read More Messages</a>
               </div>
             </li>
             <div className="topbar-divider d-none d-sm-block" />
             {/* Nav Item - User Information */}
             <li className="nav-item dropdown no-arrow">
-              <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <a className="nav-link dropdown-toggle" href="/" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span className="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
-                <img className="img-profile rounded-circle" src="img/undraw_profile.svg" />
+                <img className="img-profile rounded-circle" src="./img/avatars.avatar1.jpeg" alt='profile'/>
               </a>
               {/* Dropdown - User Information */}
               <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a className="dropdown-item" href="#">
+                <a className="dropdown-item" href="/">
                   <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400" />
                   Profile
                 </a>
-                <a className="dropdown-item" href="#">
+                <a className="dropdown-item" href="/">
                   <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400" />
                   Settings
                 </a>
-                <a className="dropdown-item" href="#">
+                <a className="dropdown-item" href="/">
                   <i className="fas fa-list fa-sm fa-fw mr-2 text-gray-400" />
                   Activity Log
                 </a>
                 <div className="dropdown-divider" />
-                <a className="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                <a className="dropdown-item" href="/" data-toggle="modal" data-target="#logoutModal">
                   <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400" />
                   Logout
                 </a>
@@ -294,7 +362,7 @@ function Navigation() {
           {/* Page Heading */}
           <div className="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
-            <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i className="fas fa-download fa-sm text-white-50" /> Generate Report</a>
+            <a href="/" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i className="fas fa-download fa-sm text-white-50" /> Generate Report</a>
           </div>
         </div>
         {/* /.container-fluid */}
